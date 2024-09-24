acb4f00c8a217add6eb0bbb247e7d8339e7fb6e6
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-24 12:17:08 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+    const location = 42
+    return Math.abs(somevalue - location)
+}
+function distanceFromHqInFeet(value){
+    const  feetBlock =264
+    const distBlock = distanceFromHqInBlocks( value);
+    return distBlock * feetBlock;
+}
+function distanceTravelledInFeet(startPart,stopPart){
+    const blockFeet = 264
+    const newDistance = Math.abs(startPart - stopPart)
+    return newDistance*blockFeet;
+}
+function  calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination)
+    if ( distance <= 400){
+        return 0
+    }
+    else if (distance > 400 && distance <=2000){
+        return (distance - 400)*0.02
+    }
+    else if (distance > 2000 && distance <=2500){
+        return 25;
+    } 
+    else {
+        return "cannot travel that far"
+    }
+}
